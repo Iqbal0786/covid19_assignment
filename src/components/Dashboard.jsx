2b61e37d85e2e5a95fecc613b93ef197cca49799
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-16 19:20:27 +0530

    added dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import "../styles/dashboard.css"
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+  <>
+   <div id='navbar'>
+      <div id='leftnav'>
+        <h5>Covid19 tracker</h5>
+      </div>
+      <div id='rightnav'>
+        <input type="text"  placeholder='type something...'/>
+        <button>
+            Search
+        </button>
+      </div>
+   </div>
+
+   <div>
+    <h1 id='title1'>Global Covid19 Information</h1>
+   <div id='box2'>
+     <div>Total Active Case :4000</div>
+     <div>Total Recovered Case :4000</div>
+     <div>Total Deceased Case :4000</div>
+   </div>
+   </div>
+  </>
   )
 }
