cf6ad17d15266a4d9db5d7e5a03c756454c574a7
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-16 18:16:53 +0530

    added routes

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -1,10 +1,17 @@
 
 import React from 'react'
-
+import {Routes, Route} from "react-router-dom"
+import Dashboard from '../components/Dashboard'
+import Login from '../components/Login'
+import Signup from '../components/Signup'
 export default function AllRoutes() {
   return (
    <>
-   
+    <Routes>
+        <Route path='/'  element={<Dashboard/>}/>
+        <Route path='/login'  element={<Login/>}/>
+        <Route path='/register'  element={<Signup/>}/>
+    </Routes>
    </>
   )
 }
